5225bd58cb7fee0bba644e93066979856300e968
0xJilan <107534852+0xJilan@users.noreply.github.com>
2023-03-09 09:50:26 +0100

    create component to simulate a defined number of trade to see effect on Pool

--- components/Simulation.js
+++ components/Simulation.js
@@ -1,33 +1,52 @@
 import { useState } from "react";
 import { Inter } from "next/font/google";
+import { getSimulation, toUSD } from "@/lib/Trade";
+
 import styles from "@/styles/Simulation.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
-const Input = ({ label, value, setValue }) => {
-  return (
-    <label>
-      {label}
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-    </label>
-  );
-};
-
 export default function Simulation() {
-  const [entryPrice, setEntryPrice] = useState("");
+  const [runs, setRuns] = useState(10);
+  const [amountInPool, setAmountInPool] = useState(30000);
+  const [simulationOutput, setSimulationOutput] = useState(null);
+  const handleClick = () => {
+    setSimulationOutput(getSimulation(runs, amountInPool));
+  };
   return (
     <>
       <section className={styles.section}>
         <h1 className={inter.className}>SIMULATION</h1>
         <div className={styles.card}>
-          <Input
-            label="Entry Price:"
-            value={entryPrice}
-            setValue={setEntryPrice}
-          />
+          <div className={styles.inputWrapper}>
+            <input
+              type="range"
+              id="runs"
+              name="runs"
+              min="10"
+              max="100"
+              defaultValue={runs}
+              onChange={(e) => setRuns(Number(e.target.value))}
+            />
+            <label htmlFor="runs">Runs: {runs}</label>
+          </div>
+
+          <div className={styles.inputWrapper}>
+            <input
+              type="range"
+              id="amountInPool"
+              name="amountInPool"
+              min="10000"
+              max="100000000"
+              defaultValue={amountInPool}
+              onChange={(e) => setAmountInPool(Number(e.target.value))}
+            />
+            <label htmlFor="runs">Amount In Pool: {toUSD(amountInPool)}</label>
+            <br />
+            <button onClick={handleClick} className={styles.buttonDesign}>
+              Run {runs} trades!
+            </button>
+          </div>
+          {simulationOutput && simulationOutput}
         </div>
       </section>
     </>
